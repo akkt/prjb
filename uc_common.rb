cb55c01381c1835837845f624a44613c9a20161d
Akiko Takano <akktkn@gmail.com>
2018-11-26 15:17:05 +0900

    for v

--- uc_common.rb
+++ uc_common.rb
@@ -29,4 +29,10 @@ module VisaLogic
   	ibmdata = iconv2.iconv(sdata)
   	ibmdata
   end
+
+  def mb_ljust(width, padding=' ')
+    output_width = each_char.map{|c| c.bytesize == 1 ? 1 : 2}.reduce(0, &:+)
+    padding_size = [0, width - output_width].max
+    self + padding * padding_size
+  end
 end
