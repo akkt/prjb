ea8fac3088ef36311bb9b61f97c45711903048dd
Akiko Takano <akktkn@gmail.com>
2018-11-29 11:08:59 +0900

    bug fixed

--- card_data_convert.rb
+++ card_data_convert.rb
@@ -6,20 +6,24 @@ class CardDataConvert
   # UC
   # 特定のフォルダ配下のファイルを処理
   Dir.glob('./UC/*').each do |target|
-  	siso_out = target.gsub(/D/, /K/)
-#  	siso_out = target.gsub(/D\d+/, "K#{target[-2,2]}")
+    siso_out = target.gsub(/D/, 'K')
+#   siso_out = target.gsub(/D\d+/, "K#{target[-2,2]}")
     Siso.for_uc(target, siso_out)
 
-    out = siso_out.gsub(/./, '')
-    Conv.convert(siso_out, out)
+    siso_out_filename = siso_out.split('/').last
+    conv_out_filename = siso_out_filename.gsub(/\./, '')
+    conv_out = siso_out.gsub(siso_out_filename, conv_out_filename)
+    Conv.convert(siso_out, conv_out)
   end
 
   # VISA
   Dir.glob('./VISA/*').each do |target|
-  	siso_out = target.gsub(/D/, /K/)
+    siso_out = target.gsub(/D/, 'K')
     Siso.for_visa(target, siso_out)
 
-    out = siso_out.gsub(/./, '')
-    Conv.convert(siso_out, out)
+    siso_out_filename = siso_out.split('/').last
+    conv_out_filename = siso_out_filename.gsub(/\./, '')
+    conv_out = siso_out.gsub(siso_out_filename, conv_out_filename)
+    Conv.convert(siso_out, conv_out)
   end
 end
